5146915bd68b7a2a62a0ccd4dc55e67841551919
Jun-Min Lee <ljm56897@gmail.com>
2025-05-16 11:13:05 +0900

    feat: Update Trans-EnV main codes

--- src/run/main.py
+++ src/run/main.py
@@ -10,11 +10,12 @@ from transformers import AutoTokenizer
 
 from torch.utils.data import DataLoader
 
-from configs import parse_args
+from configs.parse_arguments import parse_args
 from framework.guideline import return_guideline
 from framework.data_return import return_dataloader
 from framework.transformation import transformation, openai_transformation
 from registry.framework import QUESTION_KEY_ID
+from utils import log, colorstr
 from utils.common import save_func
 from utils.model_utils import return_model
 from utils.filesys_utils import pickle_load, pickle_save
@@ -24,36 +25,33 @@ choice_transform_dataset = []
 
 
 def main():
-    # initialize arguments
+    # Initialize arguments
     generation_config, model_config, dataset_config, task_config, save_config = parse_args()
 
     if dataset_config.dataset_name is None:
-        raise AssertionError('dataset name should be specified!')
+        raise AssertionError(colorstr('red', 'Dataset name should be specified!'))
     
     if task_config.task_name == 'L1' and task_config.cefr_level is None:
-        raise AssertionError('you should specify cefr level in order to change L1.')
+        raise AssertionError(colorstr('red', 'You should specify cefr level in order to change L1.'))
 
     if task_config.task_name == 'L1':
-        print(f'Dataset: {dataset_config.dataset_name}, Task: {task_config.task_name}, l1: {task_config.l1}, cefr: {task_config.cefr_level}, Rerun: {bool(generation_config.rerun)}')
+        log(f'Dataset: {colorstr(dataset_config.dataset_name)}, Task: {colorstr(task_config.task_name)}, l1: {colorstr(task_config.l1)}, cefr: {colorstr(task_config.cefr_level)}, Rerun: {colorstr(bool(generation_config.rerun))}')
     elif task_config.task_name == 'english_dialect':
-        print(f'Dataset: {dataset_config.dataset_name}, Task: {task_config.task_name}, dialect: {task_config.dialect}, Rerun: {bool(generation_config.rerun)}')
+        log(f'Dataset: {colorstr(dataset_config.dataset_name)}, Task: {colorstr(task_config.task_name)}, dialect: {colorstr(task_config.dialect)}, Rerun: {colorstr(bool(generation_config.rerun))}')
     elif task_config.task_name == 'cefr':
-        print(f'Dataset: {dataset_config.dataset_name}, Task: {task_config.task_name}, CEFR level: {task_config.cefr_level}, Rerun: {bool(generation_config.rerun)}', )
+        log(f'Dataset: {colorstr(dataset_config.dataset_name)}, Task: {colorstr(task_config.task_name)}, CEFR level: {colorstr(task_config.cefr_level)}, Rerun: {colorstr(bool(generation_config.rerun))}', )
 
-    if not os.path.exists(save_config.save_path):
-        os.makedirs(save_config.save_path)
+    os.makedirs(save_config.save_path, exist_ok=True)
 
     if dataset_config.sampling is True:
         save_config.file_name += '_sampling'
 
-
-
-    # intialize model
+    # Intialize model
     client = return_model(model_config=model_config)
     if model_config.model_name.split('/')[0] != 'azure':
-        tokenizer = AutoTokenizer.from_pretrained(model_config.model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_config.model_name, cache_dir=os.environ.get("MODEL_DIR", None))
 
-    # guideline
+    # Guideline
     guideline = return_guideline(task_config=task_config, dataset_name=dataset_config.dataset_name, data_path=save_config.data_path)
 
     to_save = list()
@@ -62,16 +60,16 @@ def main():
     # Resume
     start_idx = 0
     if os.path.exists(os.path.join(save_config.save_path, f'{save_config.file_name}.pk')):
-        print('Found existing file! Loading progress...')
+        log('Found existing file! Loading progress...')
         resume_dict = pickle_load(os.path.join(save_config.save_path, f'{save_config.file_name}.pk'))
         to_save = resume_dict['question']
         start_idx = len(to_save)
 
-    # dataloader
+    # Dataloader
     if task_config.task_name == 'cefr':
         dataset = load_dataset(
             "csv",
-            data_files = {"test": f'{save_config.data_path}/vocab_processed_2/{dataset_config.dataset_name}_{(task_config.cefr_level).lower()}.csv'},
+            data_files = {"test": f'{save_config.data_path}/assets/vocab_processed/{dataset_config.dataset_name}_{(task_config.cefr_level).lower()}.csv'},
             split="test",
         )
 
@@ -87,7 +85,7 @@ def main():
 
         dataset = load_dataset(
             "csv",  
-            data_files={"test": f'{cefr_data_path}/cefr/{dataset_config.dataset_name}/{task_config.cefr_level}.csv'},
+            data_files={"test": f'{cefr_data_path}/assets/cefr/{dataset_config.dataset_name}/{task_config.cefr_level}.csv'},
             split='test',
         )
 
@@ -108,9 +106,8 @@ def main():
         'max_tokens': generation_config.max_tokens,
     }
     
-
     for it, sample in enumerate(tqdm(dataloader)):
-        # question
+        # Question
         sentence = sample[QUESTION_KEY_ID[dataset_config.dataset_name]]                            
         sentence = [re.sub(r'_{2,}', '<blank>', s) for s in sentence]
 
@@ -136,7 +133,6 @@ def main():
         else:
             to_save_dict = {'question': to_save}
 
-
         if generation_config.rerun is None:
             pickle_save(os.path.join(save_config.save_path, f'{save_config.file_name}.pk'), to_save_dict)
         elif generation_config.rerun is not None:
@@ -149,4 +145,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
